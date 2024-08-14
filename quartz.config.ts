5b56cddca035dee58fd1b115ec6b5ffa6b6b05e5
operation-mimir <operationmimir@gmail.com>
2024-08-14 19:07:23 +0530

    Changed font

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "Noto Serif",
         code: "IBM Plex Mono",
       },
       colors: {
